f1c824bd12887bc419b3715b278ff79c5e9e59ad
huajingtong <934395737@qq.com>
2018-06-24 15:22:00 +0800

    Initial commit

---
+++ src/main/java/com/huajingtong/AutoTestDemo/App.java
@@ -0,0 +1,13 @@
+package com.huajingtong.AutoTestDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
